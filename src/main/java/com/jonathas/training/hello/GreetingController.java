beb25d4ae19f975c26be435c0304ad6728fc58ea
CGI_jonathas <jonathas.mendesalves@cgi.com>
2019-11-05 15:36:41 -0500

    Jococo Coverage - not working

--- src/main/java/com/jonathas/training/hello/GreetingController.java
+++ src/main/java/com/jonathas/training/hello/GreetingController.java
@@ -4,21 +4,19 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Api(value="Greetings Management System")
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String TEMPLATE = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @PostMapping(value = "/greeting")
     @ApiOperation(value = "View the standard welcome message", response = Greeting.class)
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+                String.format(TEMPLATE, name));
     }
 }
--- src/test/java/com/jonathas/training/hello/GreetingControllerTest.java
+++ src/test/java/com/jonathas/training/hello/GreetingControllerTest.java
@@ -20,7 +20,16 @@ public class GreetingControllerTest {
     @Test
     public void nullNameTest() {
         Greeting controller = this.greetingController.greeting(null);
-        assertEquals("Hello, World!", controller.getContent());
+        assertEquals("Hello, null!", controller.getContent());
+    }
+
+    /**
+     * If name is provided, should return the right name
+     */
+    @Test
+    public void notNullNameTest() {
+        Greeting controller = this.greetingController.greeting("Test");
+        assertEquals("Hello, Test!", controller.getContent());
     }
 
     private void t(){
